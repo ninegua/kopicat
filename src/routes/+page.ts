b345f631daba6287b32c56ae32ee5efbcee53678
Paul H. Liu <paul@thev.net>
2026-04-30 07:06:47 -0700

    Add missing page

---
+++ src/routes/+page.ts
@@ -0,0 +1,22 @@
+import { clipState } from '$lib/api/store';
+import type { ClipMode } from '$lib/api/store';
+
+export const load = async ({ url }: { url: URL }) => {
+	const path = url.pathname.replace(/\/+$/, '');
+	const hash = url.hash.slice(1);
+	const isClip = !!path;
+
+	clipState.set({
+		mode: isClip ? ('decrypt' as ClipMode) : 'create',
+		clipId: isClip ? path : null,
+		password: isClip ? hash : '',
+		decryptedText: null,
+		clip: null,
+		error: null,
+		loading: false,
+		shareUrl: null,
+		showShareModal: false,
+	});
+
+	return {};
+};
