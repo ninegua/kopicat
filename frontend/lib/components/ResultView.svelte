7949f7b45810584f6fdb2260a8333e5e7a69cc77
Paul H. Liu <paul@thev.net>
2026-05-09 03:00:08 -0700

    Change to bookmark icon

--- frontend/lib/components/ResultView.svelte
+++ frontend/lib/components/ResultView.svelte
@@ -90,9 +90,7 @@
               stroke-linecap="round"
               stroke-linejoin="round"
             >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-              <polyline points="7 10 12 15 17 10" />
-              <line x1="12" y1="15" x2="12" y2="3" />
+              <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
             </svg>
           {/if}
         </button>
